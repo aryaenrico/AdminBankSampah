1d9df33b9708186dbea39813a6a1a2fd1d9c60cc
Phaksi Bangun Asmoro <phaksibangunasmoro@gmail.com>
2022-05-04 19:20:17 +0700

    Penambahan View

--- app/src/main/java/com/aryaenrico/dynamicview/activity/TambahSampah.kt
+++ app/src/main/java/com/aryaenrico/dynamicview/activity/TambahSampah.kt
@@ -1,13 +1,28 @@
 package com.aryaenrico.dynamicview.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.aryaenrico.dynamicview.R
+import com.aryaenrico.dynamicview.databinding.ActivityDashboardBinding
+import com.aryaenrico.dynamicview.databinding.ActivityTambahSampahBinding
 
 class TambahSampah : AppCompatActivity() {
+    private lateinit var binding : ActivityTambahSampahBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambah_sampah)
         supportActionBar?.hide()
+        binding = ActivityTambahSampahBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val kategorisampah = listOf("Kertas","Besi","Plastik")
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdownitem,kategorisampah)
+        binding.filledexposed.setAdapter(arrayAdapter)
     }
+
+
+
 }
--- app/src/main/java/com/aryaenrico/dynamicview/detail/DetailPengajuan.kt
+++ app/src/main/java/com/aryaenrico/dynamicview/detail/DetailPengajuan.kt
@@ -2,11 +2,29 @@ package com.aryaenrico.dynamicview.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.aryaenrico.dynamicview.R
+import com.aryaenrico.dynamicview.databinding.ActivityDetailPengajuanBinding
+
 
 class DetailPengajuan : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailPengajuanBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_pengajuan)
+        supportActionBar?.hide()
+        binding = ActivityDetailPengajuanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val status = listOf("Disetujui","Ditolak")
+        val arrayAdapter = ArrayAdapter(this,R.layout.dropdownitem,status)
+        binding.dropDownVerifikasi.setAdapter(arrayAdapter)
+
+
+
+
+
     }
 }
