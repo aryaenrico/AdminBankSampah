6540486ac74e9dfd26f4bfb3c81a6048abde500a
Phaksi Bangun Asmoro <phaksibangunasmoro@gmail.com>
2022-07-21 11:53:31 +0700

    Final View

---
+++ app/src/main/java/com/aryaenrico/dynamicview/activity/Profile.kt
@@ -0,0 +1,12 @@
+package com.aryaenrico.dynamicview.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aryaenrico.dynamicview.R
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
